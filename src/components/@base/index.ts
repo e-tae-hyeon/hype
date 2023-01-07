bc5c5d732822300341cb5ba75de1c8b208d9e229
LTH <johtta888@naver.com>
2023-01-07 19:21:46 +0900

    Feat: 추가 card

--- src/components/@base/index.ts
+++ src/components/@base/index.ts
@@ -1,5 +1,6 @@
-export { default as SectionBase } from "./SectionBase";
 export { default as Drawer } from "./Drawer";
 export { default as Logotypo } from "./Logotypo";
 export { default as Overlay } from "./Overlay";
+export { default as SectionBase } from "./SectionBase";
 export { default as SvgIcon } from "./SvgIcon";
+export { default as Tag } from "./Tag";
---
+++ src/components/Card/BorderRadius/index.tsx
@@ -0,0 +1,19 @@
+import { Section } from "components/@module";
+import React from "react";
+import DefaultCard from "../DefaultCard";
+
+function BorderRadius() {
+  return (
+    <Section title="Border Radius">
+      <DefaultCard title="4px" className="p-4 rounded" />
+      <DefaultCard
+        title="8px"
+        description="Hype는 8px을 사용중입니다."
+        className="p-4 rounded-lg"
+      />
+      <DefaultCard title="16px" className="p-4 rounded-2xl" />
+    </Section>
+  );
+}
+
+export default BorderRadius;
