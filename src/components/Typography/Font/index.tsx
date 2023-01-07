afa142ffb53a2d3f7a768852637ec583c210cba5
LTH <johtta888@naver.com>
2023-01-07 20:13:47 +0900

    Style: 추가 to pretendard card hover effect

--- src/components/Typography/Font/index.tsx
+++ src/components/Typography/Font/index.tsx
@@ -5,7 +5,10 @@ import React from "react";
 function Font() {
   return (
     <Section title="Font">
-      <a href={PRTENDARD_URL}>
+      <a
+        href={PRTENDARD_URL}
+        className="duration-150 hover:-translate-y-1 hover:shadow-lg hover:shadow-red-500"
+      >
         <div className="p-4 text-black bg-white rounded-lg">
           <h3 className="h3">Pretendard</h3>
           <div className="body1">프리텐다드</div>
