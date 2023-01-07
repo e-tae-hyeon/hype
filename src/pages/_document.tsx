94ff08ef3575050a225fb0f757b3596cdc31fee0
LTH <johtta888@naver.com>
2023-01-07 21:06:07 +0900

    Feat: 추가 favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
+import { Favicon } from "components/@module";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <Favicon />
+      </Head>
       <body>
         <Main />
         <NextScript />
