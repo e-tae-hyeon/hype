e18eb2a442138da8b6c439734e1e0b4fbcef4366
LTH <johtta888@naver.com>
2023-01-07 20:43:08 +0900

    Feat: 추가 github repo 및 mujidog 이동

--- src/components/@module/Footer/index.tsx
+++ src/components/@module/Footer/index.tsx
@@ -1,11 +1,26 @@
-import { OFFER_EMAIL } from "common/constants/string";
+import {
+  MY_BLOG_URL,
+  MY_GITHUB_REPO_URL,
+  OFFER_EMAIL,
+} from "common/constants/string";
+import { SvgIcon } from "components/@base";
 import React from "react";
 
 function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-center caption text-neutral-400">
-      <div>made by LTH</div>
-      <div>문의 : {OFFER_EMAIL}</div>
+    <footer className="flex flex-col items-center justify-center gap-4 caption text-neutral-400">
+      <div className="flex gap-4">
+        <a href={MY_GITHUB_REPO_URL} className="p-4">
+          <SvgIcon name="github" />
+        </a>
+        <a href={MY_BLOG_URL} className="p-4">
+          <SvgIcon name="tistory" />
+        </a>
+      </div>
+      <address className="text-center">
+        <div>made by LTH</div>
+        <div>문의 : {OFFER_EMAIL}</div>
+      </address>
     </footer>
   );
 }
